1ddbc6f6d7c331b7be824c3ded3e197370571aca
Clever Sausages <58639572+CleverSausages@users.noreply.github.com>
2022-03-25 14:35:03 +0100

    Update discourse-copy-post.js
    Simplified if {}  lines 6-14  settings.min_trust_level  changed to integer

--- javascripts/discourse/api-initializers/discourse-copy-post.js
+++ javascripts/discourse/api-initializers/discourse-copy-post.js
@@ -2,14 +2,13 @@ import { apiInitializer } from 'discourse/lib/api';
 
 export default apiInitializer('0.11.1', (api) => {
   const user = api.getCurrentUser();
-
-  if (settings.min_trust_level !== 'disabled') {
+  
+  if (settings.min_trust_level !== 5) {
     if (!user) {
       return;
     }
 
-    const minTrustLevel = parseInt(settings.min_trust_level, 10);
-    if (user.trust_level < minTrustLevel) {
+    if (user.trust_level < settings.min_trust_level) {
       return;
     }
   }
